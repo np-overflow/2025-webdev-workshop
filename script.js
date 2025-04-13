07469127842d4023d08d5849c246a89109eb15cf
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-04-14 00:45:50 +0800

    Various fixes: Fix horizontal overflow in homepage, add protected page manageorders.html

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.location.href = "login.html";
+    document.getElementById("destroyButton").addEventListener("click", function () {
+        alert("Data destroyed! You have successfully ruined the lives of the employees of NullFlow Eatery.");
+    });
+});
